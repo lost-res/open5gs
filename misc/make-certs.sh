33bed8f80e4452a580116b031980dd6123495369
Sukchan Lee <acetcom@gmail.com>
2022-11-08 16:05:16 +0900

    update it

--- misc/make-certs.sh
+++ misc/make-certs.sh
@@ -17,25 +17,25 @@ openssl req -new -x509 -days 3650 -newkey rsa:2048 -nodes -keyout $1/ca.key -out
 
 # mme
 openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:2048 -out $1/mme.key
-openssl req -new -out $1/mme.csr -key $1/mme.key \
+openssl req -new -key $1/mme.key -out $1/mme.csr \
     -subj /CN=mme.localdomain/C=KO/ST=Seoul/O=NeoPlane
 openssl ca -cert $1/ca.crt -days 3650 -keyfile $1/ca.key -in $1/mme.csr -out $1/mme.crt -outdir . -batch -notext
 
 # hss
 openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:2048 -out $1/hss.key
-openssl req -new -out $1/hss.csr -key $1/hss.key \
+openssl req -new -key $1/hss.key -out $1/hss.csr \
     -subj /CN=hss.localdomain/C=KO/ST=Seoul/O=NeoPlane
 openssl ca -cert $1/ca.crt -days 3650 -keyfile $1/ca.key -in $1/hss.csr -out $1/hss.crt -outdir . -batch -notext
 
 # smf
 openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:2048 -out $1/smf.key
-openssl req -new -out $1/smf.csr -key $1/smf.key \
+openssl req -new -key $1/smf.key -out $1/smf.csr \
     -subj /CN=smf.localdomain/C=KO/ST=Seoul/O=NeoPlane
 openssl ca -cert $1/ca.crt -days 3650 -keyfile $1/ca.key -in $1/smf.csr -out $1/smf.crt -outdir . -batch -notext
 
 # pcrf
 openssl genpkey -algorithm rsa -pkeyopt rsa_keygen_bits:2048 -out $1/pcrf.key
-openssl req -new -out $1/pcrf.csr -key $1/pcrf.key \
+openssl req -new -key $1/pcrf.key -out $1/pcrf.csr \
     -subj /CN=pcrf.localdomain/C=KO/ST=Seoul/O=NeoPlane
 openssl ca -cert $1/ca.crt -days 3650 -keyfile $1/ca.key -in $1/pcrf.csr -out $1/pcrf.crt -outdir . -batch -notext
 
